218e63ab1b887c01ee5b37aac75b8e892f3a76ad
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-10-20 12:08:12 +0300

    Create TaskCounter.js

---
+++ src/components/TaskCounter/TaskCounter.js
@@ -0,0 +1,29 @@
+// Імпортуємо хук
+import { useSelector } from "react-redux";
+import { getTasks } from "../../redux/tasks/selectors";
+import * as SC from "./TaskCounter.styled";
+
+export const TaskCounter = () => {
+  // Отримуємо масив завдань із стану Redux
+  const tasks = useSelector(getTasks);
+
+  // На базі стану Redux отримуємо похідні дані
+  const count = tasks.reduce(
+    (acc, task) => {
+      if (task.completed) {
+        acc.completed += 1;
+      } else {
+        acc.active += 1;
+      }
+      return acc;
+    },
+    { active: 0, completed: 0 }
+  );
+
+  return (
+    <div>
+      <SC.Text>Active: {count.active}</SC.Text>
+      <SC.Text>Completed: {count.completed}</SC.Text>
+    </div>
+  );
+};
