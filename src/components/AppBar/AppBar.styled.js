8cc6ae572c8d6ddc7c0785298d0e92d2dcccccb5
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-10-20 12:07:41 +0300

    Create AppBar.styled.js

---
+++ src/components/AppBar/AppBar.styled.js
@@ -0,0 +1,16 @@
+import  styled  from 'styled-components'
+
+export const Wrapper = styled.header`
+    display: flax;
+    justify-content: space-between;
+`;
+
+export const Container = styled.section`
+    display: grid;
+    gap: 4px;
+`;
+
+export const Title = styled.h2`
+    margin: 0;
+    font-size: 16px;
+`;
