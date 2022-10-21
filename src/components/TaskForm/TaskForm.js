2f40f93c888593a161963811a56513bcd0f043c8
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-10-21 17:42:41 +0300

    Update TaskForm.js

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,10 +1,18 @@
 import { Button } from "components/Button/Button";
+import { useDispatch } from "react-redux";
+import { addTask } from "redux/tasks/actions";
 import * as SC from "./TaskForm.styled";
 
 export const TaskForm = () => {
+    //отримуємо посилання на ф-цію відправки екшенів
+    const dispatch = useDispatch();
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const form = event.target;
+        //Викликаємо генератор екшену та передаємо текст завдання для поля payLoad
+        //Відправляємо результат - екшен створення завдання
+        dispatch(addTask(form.elements.text.value));
         form.reset();
     };
     
