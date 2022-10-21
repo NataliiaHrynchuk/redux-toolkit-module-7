8cc804d139724787eb861dc05d2279f467073f1e
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-10-21 17:42:59 +0300

    Update actions.js

---
+++ src/redux/tasks/actions.js
@@ -0,0 +1,26 @@
+import { nanoid } from "nanoid";
+
+export const addTask = text => {
+    return {
+        type: "tasks/addTask",
+        payload: {
+            id: nanoid(),
+            completed: false,
+            text,
+        },
+    };
+};
+
+export const deleteTask = taskId => {
+    return {
+        type: "tasks/deleteTask",
+        payload: taskId,
+    };
+};
+
+export const toggleCompleted = taskId => {
+    return {
+        type: "tasks/toggleCompleted",
+        payload: taskId,
+    };
+};
