ce5ceb2f73e4d398fea0e8f5d903df958428354e
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-11-09 19:39:57 +0200

    Update AppBar.js

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,18 +1,18 @@
 import { StatusFilter } from "components/StatusFilter/StatusFilter";
 import { TaskCounter } from "components/TaskCounter/TaskCounter";
-import * as SC from "./AppBar.styled";
+import * as SC from "components/AppBar/AppBar.styled";
 
 export const AppBar = () => {
     return (
         <SC.Wrapper>
             <SC.Container>
                 <SC.Title>Tasks</SC.Title>
-                <TaskCounter/>
+                <TaskCounter />
             </SC.Container>
             <SC.Container>
                 <SC.Title>Filter by status</SC.Title>
-                <StatusFilter/>
+                <StatusFilter />
             </SC.Container>
         </SC.Wrapper>
-    )
-}
+    );
+};
