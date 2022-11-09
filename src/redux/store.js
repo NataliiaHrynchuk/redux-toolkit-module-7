7d82b7a5e48671902acd9e6178fb0b5aa5e9b1ef
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-11-09 19:40:51 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { tasksReduser } from "./tasks/reducer";
+import { tasksReducer } from "redux/tasksSlice";
 import { filtersReducer } from "redux/filterSlice";
 
 
 export const store = configureStore({
     reducer: {
-        tasks: tasksReduser,
+        tasks: tasksReducer,
         filters: filtersReducer,
     },
 });
